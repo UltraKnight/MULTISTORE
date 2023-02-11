7ebe66622da91ef36420b539fa80a52f5e51bf9c
UltraKnight <vanderlei.i.martins@outlook.com>
2023-02-11 21:56:50 +0000

    chore: update category api

--- routes/category.js
+++ routes/category.js
@@ -5,20 +5,29 @@ const Category = require('../models/Category.model');
 const User = require('../models/User.model');
 
 const requireLogin = (req, res, next) => {
-  if(req.user) {
+  if (req.user) {
     next();
   } else {
     res.status(401).json('You must be logged in to access this content');
     return;
   }
-}
+};
+
+const checkIfCategoryExists = async (name) => {
+  const foundCategory = await Category.findOne({
+    name: { $regex: new RegExp(`^${name}$`, 'i') },
+  });
+
+  return foundCategory;
+};
 
 //Get categories by user
 router.get('/categories/me', requireLogin, async (req, res) => {
   try {
-    const categoriesDB = await Category.find({createdBy: req.user._id}).populate('createdBy');
+    const categoriesDB = await Category.find({
+      createdBy: req.user._id,
+    }).populate('createdBy');
     res.status(200).json(categoriesDB);
-
   } catch (error) {
     console.log(error);
     res.status(500).json(`Error occured ${error}`);
@@ -28,8 +37,10 @@ router.get('/categories/me', requireLogin, async (req, res) => {
 //Read - only categories created by you and the admin
 router.get('/categories', requireLogin, async (req, res) => {
   try {
-    const admin = await User.findOne({username: 'admin'});
-    const categoriesDB = await Category.find({createdBy: [req.user._id, admin._id]}).populate('createdBy');
+    const admin = await User.findOne({ username: 'admin' });
+    const categoriesDB = await Category.find({
+      createdBy: [req.user._id, admin._id],
+    }).populate('createdBy');
     res.status(200).json(categoriesDB);
   } catch (error) {
     console.log(error);
@@ -51,19 +62,27 @@ router.get('/categories/all', async (_req, res) => {
 //Create
 router.post('/categories', requireLogin, async (req, res) => {
   try {
-    const {name} = req.body;
-    
-    if(!name) {
+    const { name } = req.body;
+
+    if (!name) {
       res.status(400).json(`Missing fields`);
       return;
     }
-    
+
+    const categoryExists = await checkIfCategoryExists(name);
+    if (categoryExists) {
+      res.status(400).json('Category already exists');
+      return;
+    }
+
     const category = await Category.create({
       name,
-      createdBy: req.user._id
+      createdBy: req.user._id,
     });
 
-    await User.findByIdAndUpdate(req.user._id, {$push: {categories: category.id}});
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: { categories: category.id },
+    });
     console.log('Category added to the user');
     res.status(200).json(category);
   } catch (error) {
@@ -76,13 +95,15 @@ router.post('/categories', requireLogin, async (req, res) => {
 router.delete('/categories/:id', requireLogin, async (req, res) => {
   try {
     let category = await Category.findById(req.params.id);
-    if(! category.createdBy.equals(req.user._id)) {
-      res.status(401).json('You cannot delete a category you did\'nt create.');
+    if (!category.createdBy.equals(req.user._id)) {
+      res.status(401).json("You cannot delete a category you didn't create.");
       return;
     }
 
     await Category.findByIdAndRemove(req.params.id);
-    await User.findByIdAndUpdate(req.user._id, {$pull: {categories: req.params.id}});
+    await User.findByIdAndUpdate(req.user._id, {
+      $pull: { categories: req.params.id },
+    });
     console.log('Category removed from the user');
     res.status(200).json(`Product with id ${req.params.id} was deleted.`);
   } catch (error) {
@@ -93,7 +114,7 @@ router.delete('/categories/:id', requireLogin, async (req, res) => {
 //Read - by id
 router.get('/categories/:id', async (req, res) => {
   try {
-    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
       res.status(400).json('Specified id is not valid');
       return;
     }
@@ -108,15 +129,21 @@ router.get('/categories/:id', async (req, res) => {
 //Update
 router.put('/categories/:id', requireLogin, async (req, res) => {
   try {
-    let product = await Category.findById(req.params.id);
-    if(! product.createdBy.equals(req.user._id)) {
-      res.status(401).json('You cannot edit a category you did\'nt create.');
+    let category = await Category.findById(req.params.id);
+    if (!category.createdBy.equals(req.user._id)) {
+      res.status(401).json("You cannot edit a category you did'nt create.");
       return;
     }
 
-    const productWithNewData = req.body;
-    await Category.findByIdAndUpdate(req.params.id, productWithNewData);
-    res.status(200).json(`Product with id ${req.params.id} was updated`);
+    const categoryExists = await checkIfCategoryExists(category.name);
+    if (categoryExists) {
+      res.status(400).json('A category with this name already exists.')
+      return;
+    }
+
+    const categoryWithNewData = req.body;
+    await Category.findByIdAndUpdate(req.params.id, categoryWithNewData);
+    res.status(200).json(`Category with id ${req.params.id} was updated`);
   } catch (error) {
     res.status(500).json(`Error occurred ${error}`);
   }
